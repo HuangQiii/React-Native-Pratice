d195f099d441688f3f4a7f2eec48fc0ec3e77333
HuangQiii <598260403@qq.com>
2018-03-08 23:05:52 +0800

    [FIX]

--- app/pages/LoanMore.js
+++ app/pages/LoanMore.js
@@ -26,114 +26,117 @@ export default class LoanMore extends Component {
 
     render() {
         return (
-            <ScrollView>
             <View style={styles.container}>
-                <View style={styles.header}>
-                    <View style={styles.headerTop}>
-                        <View style={styles.headerHead}>
-                            <Image resizeMode='stretch' style={{ width: 50, height: 50, }}  source={{uri:'https://modao.cc/uploads3/images/1003/10037631/raw_1496657304.png'}}/>
-                        </View>
-                        <View>
+            <ScrollView>
+                <View style={styles.container}>
+                    <View style={styles.header}>
+                        <View style={styles.headerTop}>
+                            <View style={styles.headerHead}>
+                                <Image resizeMode='stretch' style={{ width: 50, height: 50, }}  source={{uri:'https://modao.cc/uploads3/images/1003/10037631/raw_1496657304.png'}}/>
+                            </View>
                             <View>
-                                <Text style={styles.headerTitle}>小赢卡贷-2万极速放款</Text>
-                            </View>
-                            <View style={{flexDirection:'row'}}>
-                                <View style={styles.headerIcon}>
-                                    <Text style={{color:'#fff',fontSize:10,}}>放款快</Text>
+                                <View>
+                                    <Text style={styles.headerTitle}>小赢卡贷-2万极速放款</Text>
                                 </View>
-                                
+                                <View style={{flexDirection:'row'}}>
+                                    <View style={styles.headerIcon}>
+                                        <Text style={{color:'#fff',fontSize:10,}}>放款快</Text>
+                                    </View>
+                                    
+                                </View>
                             </View>
                         </View>
-                    </View>
-                    <View style={styles.des}>
-                        <Text style={{fontSize:12,}}>最低月息：0.34%</Text>
-                        <Text style={{fontSize:12,}}>贷款期限：3-12个月</Text>
-                    </View>
-                </View>
-                <View style={styles.section}>
-                    <View style={styles.instructionTitle}>
-                        <Text style={styles.borderLeft}>产品简介</Text>
-                        <Text style={styles.more}>展开</Text>
-                    </View>
-                    <Text style={{fontSize:12,}}>小赢卡贷是由小赢理财转为爱生活、重信用的你定制的信用代还产品。小赢卡贷理解你账单亮红灯的心情，独具授信快、审批快、放款快的特点，堪称信用卡充电宝。</Text>
-                </View>
-                <View style={styles.section}>
-                    <View style={styles.instructionTitle}>
-                        <Text style={styles.borderLeft}>贷款流程</Text>
-                    </View>
-                        <View style={{backgroundColor:'#fff',flexDirection:'row',}}>
-                            <IconBlock
-                                content={'信息填写'}
-                                contentSize={12}
-                                icon={'md-link'}
-                                height={60}
-                                width={60}
-                            />
-                            <IconBlock
-                                content={'实名认证'}
-                                contentSize={12}
-                                icon={'md-cafe'}
-                                height={60}
-                                width={60}
-                            />
-                            <IconBlock
-                                content={'电话审核'}
-                                contentSize={12}
-                                icon={'md-umbrella'}
-                                height={60}
-                                width={60}
-                            />
-                            <IconBlock
-                                content={'等待放款'}
-                                contentSize={12}
-                                icon={'md-glasses'}
-                                height={60}
-                                width={60}
-                            />
+                        <View style={styles.des}>
+                            <Text style={{fontSize:12,}}>最低月息：0.34%</Text>
+                            <Text style={{fontSize:12,}}>贷款期限：3-12个月</Text>
                         </View>
-                </View>
-                <View style={styles.section}>
-                    <View style={styles.instructionTitle}>
-                        <Text style={styles.borderLeft}>所需资料</Text>
                     </View>
-                        <View style={{backgroundColor:'#fff',flexDirection:'row',}}>
-                            <IconBlock
-                                content={'信用卡'}
-                                contentSize={12}
-                                icon={'md-link'}
-                                iconColor={'#FDAD2D'}
-                                height={60}
-                                width={60}
-                            />
-                            <IconBlock
-                                content={'运营商'}
-                                contentSize={12}
-                                icon={'md-cafe'}
-                                iconColor={'#5CABF8'}
-                                height={60}
-                                width={60}
-                            />
-                            <IconBlock
-                                content={'身份证'}
-                                contentSize={12}
-                                icon={'md-umbrella'}
-                                iconColor={'#00D29D'}
-                                height={60}
-                                width={60}
-                            />
+                    <View style={styles.section}>
+                        <View style={styles.instructionTitle}>
+                            <Text style={styles.borderLeft}>产品简介</Text>
+                            <Text style={styles.more}>展开</Text>
                         </View>
-                </View>
-                <View style={styles.section}>
-                    <View style={styles.instructionTitle}>
-                        <Text style={styles.borderLeft}>贷款条件</Text>
+                        <Text style={{fontSize:12,}}>小赢卡贷是由小赢理财转为爱生活、重信用的你定制的信用代还产品。小赢卡贷理解你账单亮红灯的心情，独具授信快、审批快、放款快的特点，堪称信用卡充电宝。</Text>
                     </View>
-                    <Text style={{fontSize:12,}}>满18周岁，且持有信用卡的用户</Text>
-                </View>
-                <View style={{marginTop:10,}}>
-                    <Promote />
-                </View>
-            </View >
+                    <View style={styles.section}>
+                        <View style={styles.instructionTitle}>
+                            <Text style={styles.borderLeft}>贷款流程</Text>
+                        </View>
+                            <View style={{backgroundColor:'#fff',flexDirection:'row',}}>
+                                <IconBlock
+                                    content={'信息填写'}
+                                    contentSize={12}
+                                    icon={'md-link'}
+                                    height={60}
+                                    width={60}
+                                />
+                                <IconBlock
+                                    content={'实名认证'}
+                                    contentSize={12}
+                                    icon={'md-cafe'}
+                                    height={60}
+                                    width={60}
+                                />
+                                <IconBlock
+                                    content={'电话审核'}
+                                    contentSize={12}
+                                    icon={'md-umbrella'}
+                                    height={60}
+                                    width={60}
+                                />
+                                <IconBlock
+                                    content={'等待放款'}
+                                    contentSize={12}
+                                    icon={'md-glasses'}
+                                    height={60}
+                                    width={60}
+                                />
+                            </View>
+                    </View>
+                    <View style={styles.section}>
+                        <View style={styles.instructionTitle}>
+                            <Text style={styles.borderLeft}>所需资料</Text>
+                        </View>
+                            <View style={{backgroundColor:'#fff',flexDirection:'row',}}>
+                                <IconBlock
+                                    content={'信用卡'}
+                                    contentSize={12}
+                                    icon={'md-link'}
+                                    iconColor={'#FDAD2D'}
+                                    height={60}
+                                    width={60}
+                                />
+                                <IconBlock
+                                    content={'运营商'}
+                                    contentSize={12}
+                                    icon={'md-cafe'}
+                                    iconColor={'#5CABF8'}
+                                    height={60}
+                                    width={60}
+                                />
+                                <IconBlock
+                                    content={'身份证'}
+                                    contentSize={12}
+                                    icon={'md-umbrella'}
+                                    iconColor={'#00D29D'}
+                                    height={60}
+                                    width={60}
+                                />
+                            </View>
+                    </View>
+                    <View style={styles.section}>
+                        <View style={styles.instructionTitle}>
+                            <Text style={styles.borderLeft}>贷款条件</Text>
+                        </View>
+                        <Text style={{fontSize:12,}}>满18周岁，且持有信用卡的用户</Text>
+                    </View>
+                    <View style={{marginTop:10,}}>
+                        <Promote />
+                    </View>
+                </View >
             </ScrollView>
+            <View style={{ width:width,height:50,backgroundColor:'#000', }}></View>
+            </View>
         );
     }
 }
